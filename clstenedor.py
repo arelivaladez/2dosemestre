46bf3b335e48a1e4e669c58daa1dfc38cb1c0828
arelivaladez <90646166+arelivaladez@users.noreply.github.com>
2022-05-23 18:23:09 -0500

    Add files via upload

---
+++ clstenedor.py
@@ -0,0 +1,22 @@
+class tenedor:
+    No. de picos = 4
+    color = "plata"
+    precio = "10"
+
+    def tentador(self)
+    pass 
+
+    def tenis(self) 
+    return self.color
+    
+    def precioten(self):
+        if self.precio() > 10:
+            print("Es color plata")
+        else:
+            print("Es color oro")
+chale = tenedor()
+chale.tenis()
+chale.color="plata"
+chale.precio=10
+print(chale.precio)
+chale.tentador()
