2966568ca25f9f55639cf190d1fc01ef759e836d
arelivaladez <90646166+arelivaladez@users.noreply.github.com>
2022-05-23 18:08:03 -0500

    Add files via upload

---
+++ Lap.py
@@ -0,0 +1,11 @@
+class Laptop:
+    memoriaRAM1 = "8GB"
+    Discoduro= "1T"
+    Pantalla = "13"
+
+
+    def tamaño(self):
+        return self.Pantalla
+
+    def laptococo(self):
+     print("La memoria RAM es: " + str (self.Pantalla)             
---
+++ lavadora.py
@@ -0,0 +1,18 @@
+#Lavadora 
+print("Poner ropa y detergente")
+print("Llenar agua hasta el limite")
+Luz = bool(input("¿Esta conectada?	Si/No"; ))
+
+if Luz == Si:
+	print("Colocar el tiempo de lavado e iniciar")
+	print("Vaviar agua y llevar a enjuagar")
+	print("Sacar ropa")
+	
+elif Luz == No:
+	print("Conectar lavadora")
+	print("Colocar el tiempo de lavado e iniciar")
+	print("Vaviar agua y llevar a enjuagar")
+	print("Sacar ropa")
+
+else:
+	print("Fin del programa")
